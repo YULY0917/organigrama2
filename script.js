1c2b9e55500544e508f12d54c42cac33066612f6
YULY0917 <yuly.trujillo0318@gmail.com>
2026-06-25 17:26:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
   const datos = {
   subsecretaria: {
     titulo: "Subsecretario/a",
-    nombre: "María Elisa Cabezón Otero",
-    cargo: "Subsecretaria de Previsión Social",
-    texto: `De conformidad con lo dispuesto en el artículo 41 de la ley Nº20.255 de 2008, el/la Subsecretario/a de Previsión Social es el/la jefe/a Superior de la Subsecretaría de Previsión Social, órgano de colaboración inmediata del Ministerio del Trabajo y Previsión Social.`
+    texto: `De conformidad con lo dispuesto en el artículo 41 de la ley Nº20.255 de 2008, el Subsecretario o la Subsecretaria es la jefatura superior de la Subsecretaría de Previsión Social, órgano de colaboración inmediata del Ministerio del Trabajo y Previsión Social. `
   },
 
   gabinete: {
